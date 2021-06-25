662436b5bcf57fccd4cfdaeec36e4cf1145e026f
maysaahourani <maysaahourani@gmail.com>
2021-06-25 17:16:06 +0300

    finishedlab1

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+ class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <p> The authour Name: Maysaa Alhourani</p>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
